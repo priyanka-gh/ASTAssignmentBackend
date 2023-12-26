4850e461d1eb21a277190d2cb40d092e4b2193b4
Priyanka Ghansela <priyanka.ghanselaa@gmail.com>
2023-12-26 16:51:48 +0530

    Added frontend URI

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Specify the allowed origin
+    origin: 'https://astassignment.netlify.app', // Specify the allowed origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
